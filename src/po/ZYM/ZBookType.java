58e934ed13d651fd30235c7c3a74d050de3bb816
zymzymzym <1175374753@qq.com>
2020-03-03 18:36:18 +0800

    000

--- src/po/ZYM/ZBookType.java
+++ src/po/ZYM/ZBookType.java
@@ -2,18 +2,27 @@ package po.ZYM;
 
 public class ZBookType {
     private int t_id;
+    private String t_type;
     private ZBook_Type t_type1;
+
+    public String getT_type(){
+        return t_type;
+    }
+
+    public void setT_type(String t_type) {
+        this.t_type = t_type;
+    }
 
     public int getT_id() {
         return t_id;
     }
 
-    public ZBook_Type getT_type() {
+    public ZBook_Type getT_type1() {
         return t_type1;
     }
 
-    public void setT_type(ZBook_Type t_type) {
-        this.t_type1 = t_type;
+    public void setT_type1(ZBook_Type t_type1) {
+        this.t_type1 = t_type1;
     }
 
     @Override
