2d73157d687d06a4db6f9e543e015c846714b6e5
Daijixiang <1198089555@qq.com>
2020-03-03 13:11:37 +0800

    djx

--- src/po/DJX/BookType.java
+++ src/po/DJX/BookType.java
@@ -2,17 +2,17 @@ package po.DJX;
 
 public class BookType {
     private int t_id;
-    private int t_type;
+    private String t_type;
 
     public int getT_id() {
         return t_id;
     }
 
-    public int getT_type() {
+    public String getT_type() {
         return t_type;
     }
 
-    public void setT_type(int t_type) {
+    public void setT_type(String t_type) {
         this.t_type = t_type;
     }
 
